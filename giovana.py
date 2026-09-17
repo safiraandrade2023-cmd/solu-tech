aa4ae0172ce0675cd2406dfbee1e90fe7747c4ae
safira <safiraandrade203@gmail.com>
2026-09-17 10:58:20 -0300

    adicionando processamento

--- giovana.py
+++ giovana.py
@@ -36,3 +36,46 @@ if embalagens_analisadas == 0:
 else:
     percentual_falhas = (embalagens_falha / embalagens_analisadas) * 100
 print("Percentual de falhas:", percentual_falhas)
+
+#Tipo de falha
+if embalagens_falha > 0:
+    tipo_falha = "falha na embalagem"
+else:
+    tipo_falha = "nenhuma falha encontrada"
+
+#Verificação das embalagens
+if embalagens_falha > limite_falhas:
+    alerta_embalagem = "ALERTA: quantidade de embalagens com falha acima do limite!"
+else:
+    alerta_embalagem = "quantidade de falhas dentro do limite"
+
+#Desperdicio
+if quantidade_utilizada > quantidade_produzida:
+    despercidio = quantidade_utilizada - quantidade_produzida
+else:
+    despercidio = 0
+#Valor do desperdicio
+valor_desperdicio = despercidio * preco_produto
+
+#Valor do desprdicio
+valor_desperdicio = despercidio * preco_produto
+
+#Estoque atual
+estoque_atual = estoque_anterior + quantidade_produzida
+
+#Verificação do peso
+if peso < peso_minimo:
+    alerta_peso = "ALERTA: peso abaixo do minimo permitido"
+elif peso > peso_maximo:
+    alerta_embalagem = "ALERTA: peso acima do maximo permitido"
+else:
+    alerta_peso = "Peso dentro do padrão"
+
+#Verificação da validade
+data_validade_convertida = datetime.strftime(data_validade, "%d/%m/%Y")
+hoje = datetime.now()
+
+if data_validade_convertida < hoje:
+    situacao_validade = "PRODUTO VENCIDO!"
+else:
+    situacao_validade = "produto dentro da validade"
